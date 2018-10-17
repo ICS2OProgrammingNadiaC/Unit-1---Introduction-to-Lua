837f88420a61ace4d1d8da730ce0b6ad27165229
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-10-17 14:12:03 -0400

    Update NumericTextFieldsNadiaC/main.lua

--- NumericTextFieldsNadiaC/main.lua
+++ NumericTextFieldsNadiaC/main.lua
@@ -17,6 +17,7 @@ display.setDefault("background", 124/255, 230/255, 219/255)
 -- creat local variables
 local questionObject
 local correctObject
+local incorrectObject
 local numericField
 local randomNumber1
 local randomNumber2
@@ -40,6 +41,64 @@ local function AskQuestion()
 end
 
 local function HideCorrect()
-	correctObject.isVisable = false
+	correctObject.isVisible = false
 	AskQuestion()
 end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
+	AskQuestion()
+end
+
+local function NumericFieldListener( event )
+
+	-- User begins editing "numericField"
+	if ( event.phase == "began" ) then
+
+		-- clear text field
+		event.target.text = ""
+
+	elseif event.phase == "submitted" then
+		-- when the answer is submitted (enter key is pressed) set user's input to user's answer
+		userAnswer = tonumber(event.target.text)
+
+		-- if the users answer and the correct answers are the same:
+		if (userAnswer == correctAnswer) then
+			correctObject.isVisible = true
+			timer.performWithDelay(2000, HideCorrect)
+		else incorrectObject.isVisible = true
+			timer.performWithDelay(2000, HideIncorrect)
+		end
+	end
+end
+
+------------------------------------------------------------------------------------
+-- OBJECT CREATION
+------------------------------------------------------------------------------------
+
+-- displays a question and sets it colour
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject:setTextColor(130/255, 200/255, 3/255)
+
+-- create the correct text object and make it invisable
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+correctObject:setTextColor(130/255, 30/255, 243/255)
+correctObject.isVisible = false
+
+-- create the incorrect text object and make it invisable
+incorrectObject = display.newText( "Incorrect, try again!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+incorrectObject:setTextColor(190/255, 20/255, 200/255)
+incorrectObject.isVisible = false
+-- create numeric field
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField.inputType = "number"
+
+-- add the event listener for the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener )
+
+-----------------------------------------------------------------------------------
+-- FUNCTION CALLS
+-----------------------------------------------------------------------------------
+
+-- call the function to ask the question
+AskQuestion()
