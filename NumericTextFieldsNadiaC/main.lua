cdaea91db7ce8e2210e5324fac96cb3364abacfc
Nadia <nadia.coleman@ocsbstudent.ca>
2018-10-18 13:22:35 -0700

    Update NumericTextFieldsNadiaC/main.lua

--- NumericTextFieldsNadiaC/main.lua
+++ NumericTextFieldsNadiaC/main.lua
@@ -21,31 +21,37 @@ local incorrectObject
 local numericField
 local randomNumber1
 local randomNumber2
-local userAnswer
+local userAnswer 
 local correctAnswer
 
 -------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 -------------------------------------------------------------------------------------
 
+
+
+
 local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number
-	randomNumber1 = math.random(0, 10)
-	randomNumber2 = math.random(0, 10)
+	randomNumber1 = math.random(10, 20)
+	randomNumber2 = math.random(10, 20)
 	randomOperator = math.random(1, 3)
 
-		if (randomOperator == 1) then
-			correctAnswer = randomNumber1 * randomNumber2
+	if (randomOperator == 1) then
+		correctAnswer = randomNumber1 * randomNumber2
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
 
-		elseif (randomOperator == 2) then
-			correctAnswer = randomNumber1 + randomNumber2
+	elseif (randomOperator == 2) then
+		correctAnswer = randomNumber1 + randomNumber2
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
 
- 		elseif (randomOperator == 3) then
- 			correctAnswer = randomNumber1 - randomNumber2
-		end
-
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+ 	elseif (randomOperator == 3) then
+ 		correctAnswer = randomNumber1 - randomNumber2
+ 		-- create question in text object
+ 		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+	end
 end
 
 local function HideCorrect()
@@ -56,6 +62,16 @@ end
 local function HideIncorrect()
 	incorrectObject.isVisible = false
 	AskQuestion()
+end
+
+function removeObject(userAnswer)
+	if ( incorrectObject.isVisible == false ) then
+		display.remove( userAnswer )
+		userAnswer = nil
+	else ( correctObject.isVisable = false ) then
+		display.remove( userAnswer )
+		userAnswer = nil 
+	end
 end
 
 local function NumericFieldListener( event )
@@ -73,23 +89,24 @@ local function NumericFieldListener( event )
 		-- if the users answer and the correct answers are the same:
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
-			timer.performWithDelay(2000, HideCorrect)
+			timer.performWithDelay(2500, HideCorrect)
 		else incorrectObject.isVisible = true
-			timer.performWithDelay(2000, HideIncorrect)
+			timer.performWithDelay(2500, HideIncorrect)
 		end
 	end
 end
+
 
 ------------------------------------------------------------------------------------
 -- OBJECT CREATION
 ------------------------------------------------------------------------------------
 
 -- displays a question and sets it colour
-questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/3, nil, 60 )
 questionObject:setTextColor(130/255, 200/255, 3/255)
 
 -- create the correct text object and make it invisable
-correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 60 )
 correctObject:setTextColor(130/255, 30/255, 243/255)
 correctObject.isVisible = false
 
@@ -97,9 +114,14 @@ correctObject.isVisible = false
 incorrectObject = display.newText( "Incorrect, try again!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
 incorrectObject:setTextColor(190/255, 20/255, 200/255)
 incorrectObject.isVisible = false
+
+-- display pointsObject
+pointsObject = display.newRect( display.contentWidth/1, display.contentHeight/1, 1, 50 )
+
 -- create numeric field
-numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField = native.newTextField( display.contentWidth/3, display.contentHeight/2, 150, 50 )
 numericField.inputType = "number"
+
 
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener )
