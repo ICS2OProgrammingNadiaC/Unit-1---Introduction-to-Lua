1ee98dee36bfc6950cba38e79c3daeb706715996
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-10-19 14:07:49 -0400

    Created lives and timers

--- NumericTextFieldsNadiaC/main.lua
+++ NumericTextFieldsNadiaC/main.lua
@@ -14,48 +14,32 @@ display.setDefault("background", 124/255, 230/255, 219/255)
 -- LOCAL VARIABLES
 -------------------------------------------------------------------------------------
 
--- create local variables
+-- creat local variables
 local questionObject
 local correctObject
 local incorrectObject
 local numericField
 local randomNumber1
-local randomNumber2 
-local userAnswer 
+local randomNumber2
 local userAnswer
 local numberPoints = 0
 local correctAnswer
-
------------------------------------------------------------------------------------
--- SOUNDS
------------------------------------------------------------------------------------
-
--- Correct sound
-local correctSound = audio.loadSound( "Sounds/correctSound.mp3" )
-local correctSoundChannel
-
--- Incorrect Sounds
-local incorrectSound = audio.loadSound( "Sounds/incorrectSound.mp3" )
-local incorrectSoundChannel
 
 -------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 -------------------------------------------------------------------------------------
 
-
 local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number
-	randomNumber1 = math.random(10, 20)
-	-- set randomNumber2
-	randomNumber2 = math.random(10, 20)
-	-- set randomOperator
+	randomNumber1 = math.random(0, 10)
+	randomNumber2 = math.random(0, 10)
 	randomOperator = math.random(1, 3)
-
 
 		if (randomOperator == 1) then
 			correctAnswer = randomNumber1 * randomNumber2
 			-- create question in text object
 			questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+-+
 
 		elseif (randomOperator == 2) then
 			correctAnswer = randomNumber1 + randomNumber2
@@ -65,8 +49,9 @@ local function AskQuestion()
  		elseif (randomOperator == 3) then
  			correctAnswer = randomNumber1 - randomNumber2
  			-- create question in text object
- 			questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
-	end
+			questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+		end
+
 end
 
 local function HideCorrect()
@@ -78,7 +63,6 @@ local function HideIncorrect()
 	incorrectObject.isVisible = false
 	AskQuestion()
 end
-
 
 local function NumericFieldListener( event )
 
@@ -95,15 +79,10 @@ local function NumericFieldListener( event )
 		-- if the users answer and the correct answers are the same:
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
-			correctSoundChannel = audio.play(correctSound)
 		    numberPoints = numberPoints + 1
-		    correct.text = ( "Correct = " .. numberPoints)
 			timer.performWithDelay(2000, HideCorrect)
-
+			correct.text = ( "Correct = " .. numberPoints)
 		else incorrectObject.isVisible = true
-
-			incorrectSoundChannel = audio.play(incorrectSound)
-			
 			timer.performWithDelay(2000, HideIncorrect)
 		end
 		-- clear text field
@@ -112,16 +91,17 @@ local function NumericFieldListener( event )
 end
 
 
+
 ------------------------------------------------------------------------------------
 -- OBJECT CREATION
 ------------------------------------------------------------------------------------
 
 -- displays a question and sets it colour
-questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/3, nil, 60 )
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
 questionObject:setTextColor(130/255, 200/255, 3/255)
 
 -- create the correct text object and make it invisable
-correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 60 )
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
 correctObject:setTextColor(130/255, 30/255, 243/255)
 correctObject.isVisible = false
 
@@ -129,14 +109,9 @@ correctObject.isVisible = false
 incorrectObject = display.newText( "Incorrect, try again!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
 incorrectObject:setTextColor(190/255, 20/255, 200/255)
 incorrectObject.isVisible = false
-
--- display pointsObject
-pointsObject = display.newRect( display.contentWidth/1, display.contentHeight/1, 1, 50 )
-
 -- create numeric field
-numericField = native.newTextField( display.contentWidth/3, display.contentHeight/2, 150, 50 )
-numericField.inputType = "number"
-
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
+numericField.inputType = "default"
 
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener )
